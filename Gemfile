cdf70e7f51c915622a9296a47a1b4c93f5cf4740
Charlotte Zheng <charlotte@alumni.edu.vn>
2021-06-30 18:11:47 +0800

    Add meta

--- Gemfile
+++ Gemfile
@@ -9,3 +9,9 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem "sinatra", "~> 2.1"
 
 gem "thin", "~> 1.8"
+
+gem "nokogiri", "~> 1.11"
+
+gem "open-uri", "~> 0.1.0"
+
+gem "cgi", "~> 0.2.0"
--- server.rb
+++ server.rb
@@ -1,5 +1,36 @@
 require 'sinatra'
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+require 'cgi'
 
-get '/' do
-  'Welcome'
+before do
+  content_type :json
+end
+
+get '/casts_info' do
+  dmm_url = "https://actress.dmm.co.jp"
+  cast_encoded = CGI.escape params['cast']
+  url = "#{dmm_url}/-/search/=/searchstr=#{cast_encoded}/"
+  doc = Nokogiri::HTML(URI.open(url, 'Cookie' => 'age_check_done=1'))
+  cast_info_page_url = "#{dmm_url}#{doc.css('.p-list-actress__link').attr('href')}"
+  cast_doc = Nokogiri::HTML(URI.open(cast_info_page_url, 'Cookie' => 'age_check_done=1'))
+  img_url = cast_doc.css('#main-contens img').attr('src')
+  birth_date = cast_doc.css('.p-list-profile__description:nth-child(2)').text.strip
+  spec = cast_doc.css('.p-list-profile__description:nth-child(8)').text.strip.split.join.split("cm").join
+  height = spec.scan(/B\d{2}/)[0].nil? ? "---" : spec.scan(/T\d{3}/)[0][1..3]
+  bust = spec.scan(/B\d{2}/)[0].nil? ? "---" : spec.scan(/B\d{2}/)[0][1..2]
+  cup = spec.scan(/\([A-Z]{1}/)[0].nil? ? "自行目測" : spec.scan(/\([A-Z]{1}/)[0][1]
+  waist = spec.scan(/W\d{2}/)[0].nil? ? "---" : spec.scan(/W\d{2}/)[0][1..2]
+  hip = spec.scan(/H\d{2}/)[0].nil? ? "---" : spec.scan(/H\d{2}/)[0][1..2]
+  {
+    cast_info_page_url: cast_info_page_url,
+    img_url: img_url,
+    birth_date: birth_date,
+    height: height,
+    cup: cup,
+    bust: bust,
+    waist: waist,
+    hip: hip
+  }.to_json
 end
