8d8df67381923f5f10bc003d4c100c06b4d21b13
Charlotte Zheng <jsdryan@gmail.com>
2021-07-11 21:11:43 +0800

    Rename cast to actress

--- server.rb
+++ server.rb
@@ -12,7 +12,7 @@ get '/' do
   'Welcome'
 end
 
-get '/lf_video_metadata' do
+get '/video_metadata' do
   begin
     vid_id = params['vid_id']
     libre_fanza_url = "https://www.libredmm.com/movies/#{vid_id}"
@@ -37,25 +37,25 @@ get '/lf_video_metadata' do
     else
       "https:#{javdb_specific_page_doc.css("#preview-video source").attr('src').text}"
     end
-    # Fetch casts information.
-    cast_items = doc.css(".card-title a")
-    p cast_items.count
+    # Fetch actresses information.
+    actress_items = doc.css(".card-title a")
+    p actress_items.count
 
-    casts_name = (if cast_items.count > 1
-      cast_stack = []
-      cast_items.each do |cast_item|
-        cast_stack.push(cast_item.text)
+    actresses_name = (if actress_items.count > 1
+      actress_stack = []
+      actress_items.each do |actress_item|
+        actress_stack.push(actress_item.text)
       end
-      cast_stack.join("、")
+      actress_stack.join("、")
     else
-      cast_items.text
+      actress_items.text
     end)
 
-    casts_name = "暫無演員資訊" if casts_name.empty?
+    actresses_name = "暫無演員資訊" if actresses_name.empty?
   {
     video_cover_url: video_cover_url,
     video_title: video_title,
-    casts_name: casts_name,
+    actresses_name: actresses_name,
     released_at: released_at,
     trailer_video_url: trailer_video_url
   }.to_json
@@ -66,25 +66,25 @@ get '/lf_video_metadata' do
   end
 end
 
-get '/casts_info' do
-  cast_name = params['cast']
-  cast_encoded = CGI.escape cast_name
-  libre_fanza_url = "https://www.libredmm.com/actresses?fuzzy=#{cast_encoded}"
+get '/actresses_info' do
+  actress_name = params['actress']
+  actress_encoded = CGI.escape actress_name
+  libre_fanza_url = "https://www.libredmm.com/actresses?fuzzy=#{actress_encoded}"
   doc = Nokogiri::HTML(URI.open(libre_fanza_url), nil, Encoding::UTF_8.to_s)
-  fanza_cast_code = doc.css(".card-title > a").each do |item|
-    break item.attr('href').split("/")[2] if item.text === cast_name
+  fanza_actress_code = doc.css(".card-title > a").each do |item|
+    break item.attr('href').split("/")[2] if item.text === actress_name
   end
   dmm_url = "https://actress.dmm.co.jp"
-  url = "#{dmm_url}/-/detail/=/actress_id=#{fanza_cast_code}/"
+  url = "#{dmm_url}/-/detail/=/actress_id=#{fanza_actress_code}/"
   doc = Nokogiri::HTML(URI.open(url, 'Cookie' => 'age_check_done=1'))
-  cast_info_page_url = url
-  cast_doc = Nokogiri::HTML(URI.open(cast_info_page_url, 'Cookie' => 'age_check_done=1'))
+  actress_info_page_url = url
+  actress_doc = Nokogiri::HTML(URI.open(actress_info_page_url, 'Cookie' => 'age_check_done=1'))
 
   # May not find
-  cast_id = cast_info_page_url.split("actress_id=")[1].split("/")[0]
-  img_url = cast_doc.css('#main-contens img').attr('src')
-  birth_date = if cast_doc.css('.p-list-profile__description:nth-child(2)').text.strip != "---"
-    birth_date = cast_doc.css('.p-list-profile__description:nth-child(2)').text.strip
+  actress_id = actress_info_page_url.split("actress_id=")[1].split("/")[0]
+  img_url = actress_doc.css('#main-contens img').attr('src')
+  birth_date = if actress_doc.css('.p-list-profile__description:nth-child(2)').text.strip != "---"
+    birth_date = actress_doc.css('.p-list-profile__description:nth-child(2)').text.strip
     year = birth_date.split("年")[0]
     month = birth_date.split("年")[1].split("月")[0]
     day = birth_date.split("年")[1].split("月")[1].split("日")[0]
@@ -92,15 +92,15 @@ get '/casts_info' do
   else
     "---"
   end
-  spec = cast_doc.css('.p-list-profile__description:nth-child(8)').text.strip.split.join.split("cm").join
+  spec = actress_doc.css('.p-list-profile__description:nth-child(8)').text.strip.split.join.split("cm").join
   height = spec.scan(/T\d{2}/)[0].nil? ? "---" : spec.scan(/T\d{3}/)[0][1..3]
   bust = spec.scan(/B\d{2,3}/)[0].nil? ? "---" : spec.scan(/B\d{2,3}/)[0][1..3]
   cup = spec.scan(/\([A-Z]{1}/)[0].nil? ? "---" : spec.scan(/\([A-Z]{1}/)[0][1]
   waist = spec.scan(/W\d{2}/)[0].nil? ? "---" : spec.scan(/W\d{2}/)[0][1..2]
   hip = spec.scan(/H\d{2}/)[0].nil? ? "---" : spec.scan(/H\d{2}/)[0][1..2]
   {
-    cast_info_page_url: cast_info_page_url,
-    fanza_cast_code: fanza_cast_code,
+    actress_info_page_url: actress_info_page_url,
+    fanza_actress_code: fanza_actress_code,
     img_url: img_url,
     birth_date: birth_date,
     height: height,
